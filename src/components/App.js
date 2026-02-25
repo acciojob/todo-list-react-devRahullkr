836b36a6617d62b355b1efd7dab11959943aa20b
Rahulllkr <rahulkumar8604745@gmail.com>
2026-02-25 10:58:07 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,35 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [tasklist,setTasklist] = useState([])
+  const [task,setTask] = useState("")
+
+  const handleTask = () => {
+    setTask
+    setTasklist(() => [...tasklist,task])
+  }
   return (
     <div>
         {/* Do not remove the main div */}
-        <p>Hello</p>
+        <h1>To Do List</h1>
+        <div>
+          <input onChange={(e) => setTask(e.target.value)} value={task} type="text" />
+          <button onClick={handleTask}>Add</button>
+        </div>
+        <ul>
+          {
+            tasklist.map((item,index) => {
+              return <li>
+                {item} 
+                <button>Edit</button>
+                <button>Delete</button>
+                </li>
+            })
+          }
+        </ul>
+
     </div>
   )
 }
